2c0f62ccc1425909ea9947512187b8bf4e13c674
sbx_user1051 <sbx_user1051@169.254.86.5>
2024-06-23 13:05:14 +0000

    Sun Jun 23 13:05:14 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,3 +1,7 @@
+import re
+
+regex = re.compile("[^a-zA-Z]")
+
 prices = {
     "A": 50,
     "B": 30,
@@ -9,14 +13,18 @@ prices = {
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    if type(skus) is not str:
+        return -1
+
+    cleaned_skus = regex.sub('', skus)
+    if cleaned_skus == '':
+        return
+
     
+
     # clean input
     # check for valid input
     # count items
     # apply discounts & calculate total
     pass
-
-
-
-
 
