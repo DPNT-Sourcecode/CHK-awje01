4ba4feb9432699ee38b00977eba6b040b9a8f3a9
sbx_user1051 <sbx_user1051@169.254.86.5>
2024-06-23 13:23:14 +0000

    Sun Jun 23 13:23:14 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,10 +9,21 @@ prices = {
     "D": 15
 }
 
+discounts = {
+    "A": {
+        "count": 3,
+        "price": 130
+    },
+    "B": {
+        "count": 2,
+        "price": 45
+    },
+}
+
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
+def checkout(skus: str):
     if type(skus) is not str:
         return -1
 
@@ -24,11 +35,20 @@ def checkout(skus):
         # illegal input
         return -1
 
+    total = 0
     item_names = prices.keys()
+    for item_name in item_names:
+        item_price = prices[item_name]
+        item_count = skus.count(item_name)
 
+        # apply discount
+        if item_discount := discounts.get(item_name):
+            remainder = item_count % item_discount["count"]
+            total += (remainder * item_price) + item_discount["price"] * (item_count - remainder)
 
-    # count items
-    # apply discounts & calculate total
-    pass
+        else:
+            total += item_count * item_price
+
+    return total
 
 
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -2,5 +2,6 @@ from solutions.CHK import checkout_solution
 
 
 def test_chk_r1():
-    skus = ""
-    assert checkout_solution.checkout(skus) == 3
+    skus = "AAABBC"
+    assert checkout_solution.checkout(skus) == 195
+
