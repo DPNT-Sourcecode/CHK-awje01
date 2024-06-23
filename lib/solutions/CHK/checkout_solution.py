576b3a3e2ef3a1fa7d68c4c95dca6bb27324103b
sbx_user1051 <sbx_user1051@169.254.86.5>
2024-06-23 13:45:05 +0000

    Sun Jun 23 13:45:05 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -43,22 +43,19 @@ def checkout(skus: str):
         if item_discount := discounts.get(item_name):
             remainder = item_count % item_discount["count"]
 
-            total += (remainder * item_discount["price"])
+            if remainder == 0:
+                total += item_count * item_price
 
-            total += (item_count - remainder) * item_price
-
-            # if remainder == :
-            #     total += int(item_discount["price"] * (item_count / item_discount["count"]))
-            #
-            # else:
-            #     total += remainder * item_price
-            #     total += int((item_count - remainder) / item_discount["count"]) * item_price
+            else:
+                total += remainder * item_price
+                total += (item_count - remainder) * item_price
 
         # no discounts to apply
         else:
             total += item_count * item_price
 
     return total
+
 
 
 
