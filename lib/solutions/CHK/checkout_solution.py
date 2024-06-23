1541deeba274c0cf2e846a56159eb2a2c2782fb2
sbx_user1051 <sbx_user1051@169.254.86.5>
2024-06-23 12:59:14 +0000

    Sun Jun 23 12:59:14 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,9 +1,15 @@
+prices = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15
+}
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    print(skus)
-    return 3
+
+
 
 
